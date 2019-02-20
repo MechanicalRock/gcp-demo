939ee15ba07004a54d303af7a4062b097e3db9fc
William Sia <william.sia@mechanicalrock.io>
2019-02-20 13:28:55 +0800

    Add migration script

---
+++ aspnetapp/Migrations/20190220050311_remove-data-type.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aspnetapp.Migrations
+{
+    public partial class removedatatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TodoItems",
+                table: "TodoItems");
+
+            migrationBuilder.RenameTable(
+                name: "TodoItems",
+                newName: "CloudProviders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CloudProviders",
+                table: "CloudProviders",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CloudProviders",
+                table: "CloudProviders");
+
+            migrationBuilder.RenameTable(
+                name: "CloudProviders",
+                newName: "TodoItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TodoItems",
+                table: "TodoItems",
+                column: "Id");
+        }
+    }
+}
